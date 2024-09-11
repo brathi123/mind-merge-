022dc123edbca9c0b931febe573fb0aa8b156859
brathi123 <166107374+brathi123@users.noreply.github.com>
2024-09-11 18:15:17 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int a=10;
+        int b=30;
+        int c=a+b;
+        System.out.print(c);
+    }
+}
